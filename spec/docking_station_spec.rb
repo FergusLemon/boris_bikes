89e70645900f53d54c8f2297cb5e60209e47f9d5
Fergus Lemon <fergus.lemon@gmail.com>
2018-10-25 12:42:14 +0100

    create spec for #dock

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,6 +3,7 @@ require 'bike'
 
 describe DockingStation do
   let(:docking_station) { DockingStation.new }
+  let(:bike) { Bike.new }
 
   context '#release_bike' do
     it 'recognises this message' do
@@ -10,6 +11,18 @@ describe DockingStation do
     end
     it 'releases a bike' do
       expect(docking_station.release_bike).to be_a(Bike)
+    end
+  end
+
+  context '#dock' do
+    it 'recognises this message' do
+      expect(docking_station).to respond_to(:dock).with(1).argument
+    end
+    context '1 bike has been docked' do
+      it 'has the bike in its dock' do
+        docking_station.dock(bike)
+        expect(docking_station.bikes).to include(bike)
+      end
     end
   end
 end
