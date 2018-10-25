8b8f0942f5100c31efa1ed34c2e085ebba0007c8
Fergus Lemon <fergus.lemon@gmail.com>
2018-10-25 10:54:34 +0100

    Use let declaration instead of subject

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,4 +1,9 @@
 require 'docking_station'
 describe DockingStation do
-
+  let(:docking_station) { DockingStation.new }
+  context '#release_bike' do
+    it 'recognises this message' do
+      expect(docking_station).to respond_to(:release_bike)
+    end
+  end
 end
