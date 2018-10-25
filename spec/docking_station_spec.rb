b15b827f4883d9bc0e58ecdc78c71441ec9979f7
Fergus Lemon <fergus.lemon@gmail.com>
2018-10-25 11:18:06 +0100

    Add spec to check a bike instance is released

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,9 +1,15 @@
 require 'docking_station'
+require 'bike'
+
 describe DockingStation do
   let(:docking_station) { DockingStation.new }
+
   context '#release_bike' do
     it 'recognises this message' do
       expect(docking_station).to respond_to(:release_bike)
+    end
+    it 'releases a bike' do
+      expect(docking_station.release_bike).to be_a(Bike)
     end
   end
 end
