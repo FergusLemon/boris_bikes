3a8fad9aba7f71bd1b95a3b7e8bc7c7ca96887b9
Fergus Lemon <fergus.lemon@gmail.com>
2018-10-27 10:41:03 +0100

    Update #collect_bikes to accept bikes from a location and store in @bikes array.

--- lib/van.rb
+++ lib/van.rb
@@ -1,6 +1,10 @@
 class Van
+attr_reader :bikes
 
+  def initialize
+    @bikes = []
+  end
   def collect_bikes(location)
-
+    bikes << location.handover_bikes
   end
 end
--- spec/van_spec.rb
+++ spec/van_spec.rb
@@ -2,10 +2,18 @@ require 'van'
 
 describe Van do
   let(:van) { described_class.new }
+  let(:broken_bike) { double :bike }
+  let(:location) { double("docking station", handover_bikes: [broken_bike]) }
 
   describe '#collect_bikes' do
     it 'recognises this message' do
       expect(van).to respond_to(:collect_bikes).with(1).argument
+    end
+    context 'when there are bikes to collect' do
+      it 'collects the bikes' do
+        van.collect_bikes(location)
+        expect(van.bikes).to eq([[broken_bike]])
+      end
     end
   end
 end
