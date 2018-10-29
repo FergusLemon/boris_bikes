6cb158067d42459531877f6c1595e6a242945f95
Fergus Lemon <fergus.lemon@gmail.com>
2018-10-29 08:45:50 +0000

    Test for existence of #receive_bikes.

--- spec/garage_spec.rb
+++ spec/garage_spec.rb
@@ -1,5 +1,11 @@
 require 'garage'
 
 describe Garage do
-  let(:garage) { described.class.new }
+  let(:garage) { described_class.new }
+
+  describe '#receive_bikes' do
+    it 'recognises this message' do
+      expect(garage).to respond_to(:receive_bikes)
+    end
+  end
 end
