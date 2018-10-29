e41093e17e5d70c7cb8e892c0eddd4a7d7fef47d
Fergus Lemon <fergus.lemon@gmail.com>
2018-10-29 12:59:16 +0000

    Introduce capacity to vans, create tests and implement code.

--- lib/van.rb
+++ lib/van.rb
@@ -1,8 +1,10 @@
 class Van
-attr_reader :bikes
+attr_reader :bikes, :capacity
+DEFAULT_CAPACITY = 20
 
-  def initialize
+  def initialize(capacity = DEFAULT_CAPACITY)
     @bikes = []
+    @capacity = capacity
   end
 
   def collect_bikes(location)
