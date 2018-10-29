bc72bae148c534a49c4bdeb0f7e53eceba2d3f04
Fergus Lemon <fergus.lemon@gmail.com>
2018-10-29 14:20:53 +0000

    Create specs for #add_bike  and implement method.

--- lib/bike_container.rb
+++ lib/bike_container.rb
@@ -1,3 +1,20 @@
 module BikeContainer
+attr_reader :capacity, :bikes
+DEFAULT_CAPACITY = 20
 
+  def initialize(capacity = DEFAULT_CAPACITY)
+    @capacity = capacity
+    @bikes = []
+  end
+
+  def add_bike(bike)
+    raise "This #{self.class} is full." if full?
+    bikes << bike
+  end
+
+  private
+
+  def full?
+    bikes.size == capacity
+  end
 end
--- spec/support/shared_examples_bike_container.rb
+++ spec/support/shared_examples_bike_container.rb
@@ -1,4 +1,48 @@
 require 'bike_container'
 shared_examples_for BikeContainer do
   let(:bike_container) { described_class.new }
+  let(:bike) { double("bike") }
+
+  describe '#initialize' do
+    context 'when no capacity is given' do
+      it 'takes the default capacity' do
+        expect(bike_container.capacity).to eq(described_class::DEFAULT_CAPACITY)
+      end
+    end
+    context 'when a capacity is given' do
+      it 'overrides the default capacity' do
+        random_capacity = rand(100)
+        random_capacity_bike_container = described_class.new(random_capacity)
+        expect(random_capacity_bike_container.capacity).to eq(random_capacity)
+      end
+    end
+  end
+
+  describe '#add_bike' do
+    it 'recognises this message' do
+      expect(bike_container).to respond_to(:add_bike).with(1).argument
+    end
+    context 'when one bike has been added' do
+      it 'contains the bike' do
+        bike_container.add_bike(bike)
+        expect(bike_container.bikes).to include(bike)
+      end
+    end
+    context 'when default capacity is used' do
+      it 'has a capacity of twenty' do
+        expect { described_class::DEFAULT_CAPACITY.times { bike_container.add_bike(bike) } }.not_to raise_error
+      end
+      it 'does not accept bikes when full' do
+        described_class::DEFAULT_CAPACITY.times { bike_container.add_bike(bike) }
+        expect { bike_container.add_bike(bike) }.to raise_error(RuntimeError, "This BikeContainerTest is full.")
+      end
+    end
+    context 'when a user defined capacity is used' do
+      it 'overrides the defualt capacity' do
+        random_capacity = rand(100)
+        random_capacity_bike_container = described_class.new(random_capacity)
+        expect { random_capacity.times { random_capacity_bike_container.add_bike(bike) } }.not_to raise_error
+      end
+    end
+  end
 end
