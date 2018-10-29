31ba8a1d322cee81987f9b8c812d0c02b4043b65
Fergus Lemon <fergus.lemon@gmail.com>
2018-10-29 08:47:27 +0000

    Write test example expecting a van to deliver bikes to a garage.

--- spec/van_spec.rb
+++ spec/van_spec.rb
@@ -4,7 +4,7 @@ describe Van do
   let(:van) { described_class.new }
   let(:broken_bike) { double :bike }
   let(:docking_station) { double("docking station", handover_bikes: [broken_bike]) }
-  let(:garage) { double :garage }
+  let(:garage) { double("garage", receive_bikes: [broken_bike]) }
 
   describe '#collect_bikes' do
     it 'recognises this message' do
@@ -21,6 +21,13 @@ describe Van do
   describe '#deliver_bikes' do
     it 'recognises this message' do
       expect(van).to respond_to(:deliver_bikes).with(1).argument
+    end
+    context 'when there are bikes to deliver' do
+      it 'delivers the bikes' do
+        van.collect_bikes(docking_station)
+        van.deliver_bikes(garage)
+        expect(van.bikes).to be_empty
+      end
     end
   end
 end
